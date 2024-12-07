388496488a464aa5e0e06188ce863fcd20117d76
nakul-krishnakumar <nakulkrishnakumar86@gmail.com>
2024-12-07 17:52:09 +0000

    Setup frontend with vite

---
+++ app/src/App.tsx
@@ -0,0 +1,10 @@
+
+const App: React.FC = () => {
+  return (
+    <>
+      <div className="container" style={{width: '200px', height: '300px', backgroundColor: 'var(--base-primary)'}}></div>
+    </>
+  )
+}
+
+export default App;
